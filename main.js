2aaab2eed553af60880cf3c100c75836b4b50d9b
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2020-12-11 23:09:56 +0100

    first change

--- main.js
+++ main.js
@@ -1,2 +1,57 @@
-I am so cool, uwu
-asdasdasasd
+//-----------------BACKEND---VARS----------------
+let userLogin;
+let userPassword;
+//----------------------------------------------
+
+
+// OBIEKT STRONY
+class Page {
+    constructor(content,id){  
+        this.content = content;
+        this.id = id;
+    }
+};
+
+page = [];
+page[0] = new Page("Witamy");
+page[1] = new Page("Pierwsza strona",1);
+page[2] = new Page("Druga",2);
+page[3] = new Page("Trzecia",3);
+page[4] = new Page("Cztery",4);
+page[5] = new Page("Piec",5);
+page[6] = new Page("Szesc",6);
+//ZMIENIANIE ZAWARTOSCI NOTESU
+function openPage(page) {
+    mainSpace.innerHTML = page.content;
+}
+
+// DODAWANIE EVENTLISTENEROW - POTEM DO STARTU DODAC
+function addListenersLeftMenu() {
+    for (let i = 1; i < 7; i++) {
+        let item = document.getElementById("leftMenuElement" + i);
+        item.addEventListener("click", function () {
+            openPage(page[i]);
+        });
+    }
+}
+
+// LOGOWANIE
+
+function showLoginBox(){
+    let item = document.getElementById("loginBox"); 
+    let bg = document.getElementById("mainContainer");
+    item.style.display = "block";
+    bg.style.filter = "blur(8px)";
+}
+
+window.onload = function(){
+    addListenersLeftMenu();
+}
+
+//---------------ONCLICKS-------------
+document.getElementById("homeButton").addEventListener("click",function(){
+    openPage(page[0]);
+})
+document.getElementById("loginBox").addEventListener("click",function(){
+    showLoginBox();
+})
