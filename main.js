816324892aee18aecf7c673de2901f797e7baa65
CleverDarkSolutions <66365110+CleverDarkSolutions@users.noreply.github.com>
2020-12-12 02:02:33 +0100

    KEKW

--- main.js
+++ main.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-=======
-//-----------------BACKEND---VARS----------------
-
-//----------------------------------------------
-
-//-------------INNER--HTMLS-------------------------
-page1 = "<input>";
-
-
->>>>>>> parent of 80a08de... commit
 // OBIEKT STRONY
 class pageObj {
     constructor(content,id){  
@@ -17,7 +6,6 @@ class pageObj {
     }
 };
 
-<<<<<<< HEAD
 let page = [];
 page[0] = new pageObj("Witamy",0);
 page[1] = new pageObj("Pierwsza",1);
@@ -26,16 +14,6 @@ page[3] = new pageObj("Trzecia",3);
 page[4] = new pageObj("Cztery",4);
 page[5] = new pageObj("Piec",5);
 page[6] = new pageObj("Szesc",6);
-=======
-page = [];
-page[0] = new Page("Witamy");
-page[1] = new Page(page1,1);
-page[2] = new Page("Druga",2);
-page[3] = new Page("Trzecia",3);
-page[4] = new Page("Cztery",4);
-page[5] = new Page("Piec",5);
-page[6] = new Page("Szesc",6);
->>>>>>> parent of 80a08de... commit
 //ZMIENIANIE ZAWARTOSCI NOTESU
 function openPage(page) {
     mainSpace.innerHTML = page.content;
@@ -43,7 +21,7 @@ function openPage(page) {
 
 // DODAWANIE EVENTLISTENEROW - POTEM DO STARTU DODAC
 function addListenersLeftMenu() {
-    for (let i = 1; i < 7; i++) {
+    for (let i = 1; i < 6; i++) {
         let item = document.getElementById("leftMenuElement" + i);
         item.addEventListener("click", function () {
             openPage(page[i]);
@@ -67,19 +45,15 @@ function quitLoginBox(){
     bg.style.filter = "none";
 }
 
-<<<<<<< HEAD
 function noteScreen(){
     let item = document.getElementById("mainSpace");
     let subjects = ["Angielski","UKOS","WIA"];
     for(let i=0;i<subjects.length;i++){
         let oneSubject = "<div class='subject'>"+subjects[i]+"</div>";
+        console.log(oneSubject);
         item.innerHTML += oneSubject;
     }
     item.style.display = "block";
-=======
-function addTask(){
- 
->>>>>>> parent of 80a08de... commit
 }
 
 //---------------ONCLICKS-------------
