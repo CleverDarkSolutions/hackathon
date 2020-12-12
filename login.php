8e080a941b9f7d911b8909ec645fa3f6b87dd3f9
Chili0xFF <75846282+Chili0xFF@users.noreply.github.com>
2020-12-12 02:11:40 +0100

    123

--- login.php
+++ login.php
@@ -7,7 +7,7 @@
     $db_pass = '';
     $connect = mysqli_connect('localhost',$db_user,$db_pass);
 	mysqli_select_db($connect,$db_name) or die(mysqli_error($connect));
-	$query="SELECT * FROM uzytkownik WHERE login like'$login' AND haslo like '$haslo';"
+	$query="SELECT * FROM uzytkownik WHERE login like'$login' AND haslo like '$haslo';";
 	if(mysqli_query($connect,$query))
 	{
 		session_start();
